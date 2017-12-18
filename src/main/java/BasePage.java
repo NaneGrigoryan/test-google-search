a7329f21898f1ce3977518593ca8bdba18eab09f
Tigran Ghabuzyan <tigran.ghabuzyan@gmail.com>
2017-12-18 20:56:46 +0400

    inital

---
+++ src/main/java/BasePage.java
@@ -0,0 +1,60 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+    private WebDriver driver;
+
+    public BasePage(WebDriver webdriver){
+        this.driver = webdriver;
+    }
+    public void visit(String url){
+        driver.get(url);
+    }
+    public WebElement find(By locator){
+        return driver.findElement(locator);
+        //return find(By.cssSelector(cssSelector));
+    }
+    public WebElement find(String cssSelector){
+        return find(By.cssSelector(cssSelector));
+    }
+
+    public void click(By locator){
+        click(find(locator));
+        //find (locator).click();
+    }
+    public void click(WebElement element){
+        element.click();
+    }
+    public void click(String cssSelector) {
+        click(driver.findElement(By.cssSelector(cssSelector)));
+        //click(find(cssSelector));
+    }
+    public void type(By locator, String text){
+        type(find(locator), text); // aveli lav tarberak qan nerqevin@
+        //find(locator).sendKeys(text);
+    }
+    public void type(WebElement element, String text){
+        element.sendKeys(text);
+    }
+    public void type(String cssSelector, String text){
+        type(find(cssSelector), text);
+    }
+    public boolean isDisplayed(WebElement element){
+        try {
+            return element.isDisplayed();
+        } catch (NoSuchElementException e){
+            return false;
+        }
+    }
+    public boolean isDisplayed(By locator){
+        return isDisplayed(find(locator));
+    }
+    public boolean isDisplayed(String cssSelector){
+        return isDisplayed(find(cssSelector));
+    }
+
+}
