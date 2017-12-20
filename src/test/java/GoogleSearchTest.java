28fc4b1fd0ec4370c4a8a9392cbd7208bd9b3e74
Tigran Ghabuzyan <tigran.ghabuzyan@gmail.com>
2017-12-21 02:04:01 +0400

    draft

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -1,3 +1,26 @@
-public class GoogleSearchTest {
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+public class GoogleSearchTest {
+    private GoogleSearchPage googleSearchPage;
+    private ChromeDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\dev\\chromedriver.exe");
+        driver = new ChromeDriver();
+        googleSearchPage = new GoogleSearchPage(driver);
+    }
+    @Test
+    public void searchTest(){
+
+
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
 }
