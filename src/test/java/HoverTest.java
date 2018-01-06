fdf1cce16570379896526e762a2170308e5f87ff
Tigran Ghabuzyan <tigran.ghabuzyan@gmail.com>
2018-01-06 18:27:04 +0400

    Hover

---
+++ src/test/java/HoverTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class HoverTest {
+    private ChromeDriver driver;
+    private HoverPage hoverPage;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\dev\\chromedriver.exe");
+        driver = new ChromeDriver();
+        hoverPage = new HoverPage(driver);
+
+    }
+/*
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+*/
+    @Test
+    public void dropdownSelect() {
+        assertTrue(hoverPage.isHeaderNotDisplayed());
+        hoverPage.hoverAvatar();
+        assertTrue(hoverPage.isHeaderDisplayed());
+    }
+
+}
